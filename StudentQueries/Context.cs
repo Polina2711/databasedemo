b558ba0deb2fbfe8472bcd600a5ad603c3996ca5
strorinWind <sakunchik1@yandex.ru>
2016-11-07 15:56:44 +0300

    break

--- StudentQueries/Context.cs
+++ StudentQueries/Context.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace StudentQueries
 {
-	class Context : DbContext
+	class GreenContext : DbContext
 	{
 		public DbSet<Student> Students { get; set; }
 		public DbSet<Group> Groups { get; set; }
 
-		public Context() : base("localsql")
+		public GreenContext() : base("localsql")
 		{
 
 		}
